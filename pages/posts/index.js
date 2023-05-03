6ecc76808e8b126e0d819e3ec215b9473c71744a
vasil.krumov <vassil.krumov@gmail.com>
2023-05-03 11:28:16 +0300

    Slug Posts Completed

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,17 @@
 import AllPosts from '../../components/posts/all-posts'
-import { DUMMY_POSTS } from '../index'
+import { getAllPosts } from '../../lib/posts-util'
 
-function AllPostsPage() {
-    return <AllPosts posts={DUMMY_POSTS} />
+function AllPostsPage(props) {
+    return <AllPosts posts={props.posts} />
 }
 
+export function getStaticProps() {
+    const allPosts = getAllPosts()
+
+    return {
+        props: {
+            posts: allPosts,
+        },
+    }
+}
 export default AllPostsPage
