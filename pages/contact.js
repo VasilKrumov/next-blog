aad55b8b98f83a692e7e3a959e52fcccfbcf03f4
vasil.krumov <vassil.krumov@gmail.com>
2023-05-04 11:05:18 +0300

    Api Routes Added

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,7 @@
+import ContactForm from '../components/contact/contact-form'
+
 function ContactPage() {
-    return (
-        <div>
-        <h1>The Contact Page</h1>
-        </div>
-    );
+    return <ContactForm />
 }
+
+export default ContactPage
